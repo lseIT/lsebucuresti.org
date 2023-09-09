b35f3294f6aecfacecc39323ec562a54def95693
MDGcode <adrianboscanici@yahoo.com>
2023-09-09 22:14:20 +0300

    Added tailwind

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import {useState} from 'react';
+import React from "react";
+import { useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState(1);
 
-    const [data, setData] = useState(1);
+  const handler = () => {
+    setData(data + 1);
+  };
 
-    const handler = () => {
-        setData(data + 1);
-    }
-
-    return (
-        <div>
-            <a href="/">Home</a>
-            <p></p>
-            <a href="/membrii">Membrii</a>
-            <p> Count: {data}</p>
-            <button onClick={handler}>Click me</button>
-        </div>
-    );
-
+  return (
+    <div>
+      <a href="/">Home</a>
+      <p></p>
+      <a href="/membrii">Membrii</a>
+      <p> Count: {data}</p>
+      <button onClick={handler}>Click me</button>
+    </div>
+  );
 }
